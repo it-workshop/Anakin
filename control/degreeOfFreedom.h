b68a9d13add47c39d83b049dbb269112bc5a71d0
VladimirGl <glazachev.vladimir@gmail.com>
2013-09-11 23:31:35 +0400

    added implementation of degreeOfFreedom class

---
+++ control/degreeOfFreedom.h
@@ -0,0 +1,30 @@
+#ifndef DEGREEOFFREEDOM_H
+#define DEGREEOFFREEDOM_H
+
+/**
+* @file degreeOfFreedom.h
+*
+* Implementation of DegreeOfFreedom class.
+* Contains two values, which varies within the flex sensor datas.
+*/
+
+class DegreeOfFreedom
+{
+public:
+	DegreeOfFreedom(int newMin, int newMax) :
+		minimum(newMin),
+		maximum(newMax)
+	{}
+
+	void setMin(int const& newMin) { minimum = newMin; }
+	void setMax(int const& newMax) { maximum = newMax; }
+
+	void min() const { return minimum; }
+	void max() const { return maximum; }
+
+private:
+	int minimum;
+	int maximum;
+};
+
+#endif // DEGREEOFFREEDOM_H
