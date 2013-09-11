1c1b4cf39e1ab9f8df6bb8f69d5c8d4f41ef056a
VladimirGl <glazachev.vladimir@gmail.com>
2013-09-12 01:19:18 +0400

    need implementation of abstract glove class

---
+++ control/abstractGlove.h
@@ -0,0 +1,12 @@
+#ifndef ABSTRACTGLOVE_H
+#define ABSTRACTGLOVE_H
+
+// надо что-бы Glove и GhostGlove наследовались отсюда.
+
+class AbstractGlove
+{
+public:
+	AbstractGlove();
+};
+
+#endif // ABSTRACTGLOVE_H
