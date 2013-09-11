28124fc7de478d951345dcd24dcd5308f9aad40d
VladimirGl <glazachev.vladimir@gmail.com>
2013-09-11 23:09:14 +0400

    implementation of gloveInterface class, used to communication with Glove class

--- control/gloveInterface.cpp
+++ control/gloveInterface.cpp
@@ -1,5 +1,46 @@
 #include "gloveInterface.h"
 
-GloveInterface::GloveInterface()
+#include "glove.h"
+
+GloveInterface::GloveInterface() :
+	mGlove(new Glove)
 {
+}
+
+GloveInterface::~GloveInterface()
+{
+	if (isHardwareGloveSet()) {
+		QObject::disconnect(mGlove, SIGNAL(dataIsRead()), this, SIGNAL(dataIsRead()));
+	}
+
+	delete mGlove;
+}
+
+void GloveInterface::setHardwareGlove()
+{
+	mGlove->connectHardwareGlove();
+
+	if (isHardwareGloveSet()) {
+		QObject::connect(mGlove, SIGNAL(dataIsRead()), this, SIGNAL(dataIsRead()));
+	}
+}
+
+bool GloveInterface::isHardwareGloveSet()
+{
+	return mGlove->isPortSet();
+}
+
+void GloveInterface::startSendingDatas()
+{
+	mGlove->startSendingData();
+}
+
+void GloveInterface::stopSendingDatas()
+{
+	mGlove->stopSendingData();
+}
+
+QList<int> *GloveInterface::gloveDatas()
+{
+	mGlove->data();
 }
--- control/gloveInterface.h
+++ control/gloveInterface.h
@@ -1,10 +1,41 @@
 #ifndef GLOVEINTERFACE_H
 #define GLOVEINTERFACE_H
 
-class GloveInterface
+#include <QObject>
+#include <QList>
+
+/**
+* @file gloveInterface.h
+*
+* Implementation of GloveInterface class.
+* All interaction with the glove comes through this class.
+*/
+
+class Glove;
+
+class GloveInterface : public QObject
 {
+	Q_OBJECT
 public:
 	GloveInterface();
+	~GloveInterface();
+
+	/// Trying to connect mGlove with a real glove.
+	void setHardwareGlove();
+	/// Returns true if mGlove serial port set.
+	bool isHardwareGloveSet();
+
+	void startSendingDatas();
+	void stopSendingDatas();
+
+	QList<int> *gloveDatas();
+
+signals:
+	/// Re emits the signal of the glove.
+	void dataIsRead();
+
+private:
+	Glove *mGlove;
 };
 
 #endif // GLOVEINTERFACE_H
