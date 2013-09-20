9590fbdb9c0e8bb1c37b592cb272788a624feb90
VladimirGl <glazachev.vladimir@gmail.com>
2013-09-20 20:52:08 +0400

    implementation of hand interface class

--- control/handInterface.cpp
+++ control/handInterface.cpp
@@ -1,5 +1,51 @@
 #include "handInterface.h"
 
-HandInterface::HandInterface()
+#include "hand.h"
+
+HandInterface::HandInterface() :
+	mHand(new Hand)
 {
+}
+
+HandInterface::~HandInterface()
+{
+	if (isHardwareHandSet()) {
+		QObject::disconnect(mHand, SIGNAL(dataIsRead()), this, SIGNAL(dataIsRead()));
+	}
+
+	delete mHand;
+}
+
+void HandInterface::setHardwareHand()
+{
+	mHand->connectHardwareHand();
+
+	if (isHardwareHandSet()) {
+		QObject::connect(mHand, SIGNAL(dataIsRead()), this, SIGNAL(dataIsRead()));
+	}
+}
+
+bool HandInterface::isHardwareHandSet()
+{
+	return mHand->isPortSet();
+}
+
+void HandInterface::startSendingDatas()
+{
+	mHand->startSendingData();
+}
+
+void HandInterface::stopSendingDatas()
+{
+	mHand->stopSendingData();
+
+}
+void HandInterface::moveMotor(const int &num, const int &value)
+{
+	mHand->moveMotor(num, value);
+}
+
+QList<int> *HandInterface::motorsDatas()
+{
+	mHand->currentPosition();
 }
--- control/handInterface.h
+++ control/handInterface.h
@@ -1,10 +1,46 @@
 #ifndef HANDINTERFACE_H
 #define HANDINTERFACE_H
 
-class HandInterface
+#include <QObject>
+#include <QList>
+
+/**
+* @file handInterface.h
+*
+* Implementation of GloveInterface class.
+* All interaction with the hand comes through this class.
+*/
+
+class Hand;
+
+class HandInterface : public QObject
 {
+	Q_OBJECT
 public:
 	HandInterface();
+	~HandInterface();
+
+	/// Trying to connect mHand with a real glove.
+	void setHardwareHand();
+	/// Returns true if mHand serial port set.
+	bool isHardwareHandSet();
+
+	/// Calls the appropriate method of the Hand class.
+	void startSendingDatas();
+	/// Calls the appropriate method of the Hand class.
+	void stopSendingDatas();
+
+	/// Request hardware hand to set servo number num to value.
+	void moveMotor(int const& num, int const& value);
+	/// Returns last datas from the servos.
+	QList<int> *motorsDatas();
+
+signals:
+	/// Re emits the signal of the glove.
+	void dataIsRead();
+
+private:
+	Hand *mHand;
 };
 
 #endif // HANDINTERFACE_H
